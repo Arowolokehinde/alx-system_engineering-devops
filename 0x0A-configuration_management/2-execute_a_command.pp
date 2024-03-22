174f8d57e34f67584834ed551d3dde13496426e3
Arowoolo kehinde <arowolokehinde321@gmail.com>
2024-03-22 12:40:30 -0400

    a manifest that kills a process named killmenow.

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
-# a manifest that kills a process named killmenow.
+# kill process killmenow
 
 exec { 'pkill':
-  command => 'pkill killmenow',
+  command  => 'pkill killmenow',
   provider => 'shell',
 }
